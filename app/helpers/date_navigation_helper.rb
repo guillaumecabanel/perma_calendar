ebdcc60d8524efa70529e1709d8d185795b3a6f2
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2019-05-27 11:55:14 +0200

    update nav between date

--- app/helpers/date_navigation_helper.rb
+++ app/helpers/date_navigation_helper.rb
@@ -1,7 +1,21 @@
 module DateNavigationHelper
-  def link_to_today_advice
-    if DateTime.current < params[:from] || DateTime.current > params[:to]
-      link_to "Aujourd'hui", advices_path({ garden: params[:garden] }.merge(date: (DateTime.current).strftime( '%d-%m-%Y')))
+  def previous_date_navigation_link
+    if DateTime.current < params[:from]
+      link_to "Aujourd'hui", advices_path({ garden: params[:garden] }.merge(date: (DateTime.current).strftime( '%d-%m-%Y'))), class: 'button date-nav-link prev'
+    elsif DateTime.current < params[:from] + 5.days
+      link_to 'Quinzaine précédente', advices_path({ garden: params[:garden] }.merge(date: (params[:from] - 15.days).strftime( '%d-%m-%Y'))), class: 'button date-nav-link prev'
+    else
+      tag.div class: 'empty-nav-link'
+    end
+  end
+
+  def next_date_navigation_link
+    if DateTime.current > params[:to]
+      link_to "Aujourd'hui", advices_path({ garden: params[:garden] }.merge(date: (DateTime.current).strftime( '%d-%m-%Y'))), class: 'button date-nav-link next'
+    elsif DateTime.current > params[:to] - 5.days
+      link_to 'Quinzaine suivante', advices_path({ garden: params[:garden] }.merge(date: (params[:to] + 15.days).strftime( '%d-%m-%Y'))), class: 'button date-nav-link next'
+    else
+      tag.div class: 'empty-nav-link'
     end
   end
 end
